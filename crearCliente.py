c29ebe0127c27b8d6cf80082ed7a64257187ea44
franciscoMigliardi24 <francisco.migliardi@comunidad.ub.edu.ar>
2020-02-13 18:21:06 -0300

    arreglos de creacion se buscaba un valor previo para despues ingresar el valor correspondiente

--- crearCliente.py
+++ crearCliente.py
@@ -28,99 +28,50 @@ driver.find_element_by_class_name('boton-1').click()
 #abrir modal para crear cliente
 driver.find_element_by_class_name('agregarCliente').click()
 
-
-
 #ingresar nombre del cliente
 element = driver.find_element_by_name("nombreCliente")
-value = element.get_attribute("value")
-if value == "cliente1":
-	print("el nombre esta bien")
-else:
-	print("el nombre no esta bien")
-	driver.find_element_by_name("nombreCliente").clear()
-	driver.find_element_by_name("nombreCliente").send_keys("cliente1")
+element.send_keys("cliente1")
 
 #ingresar ciudad
 element = driver.find_element_by_name("ciudad")
-value = element.get_attribute("value")
-if value == "Buenos Aires":
-	print("la ciudad esta bien")
-else:
-	print("la ciudad no esta bien")
-	driver.find_element_by_name("ciudad").clear()
-	driver.find_element_by_name("ciudad").send_keys("Buenos Aires")
+element.send_keys("Buenos Aires")
 
 #ingresar estado
 element = driver.find_element_by_name("estado")
-value = element.get_attribute("value")
-if value == "Capital Federal":
-	print("el estado esta bien")
-else:
-	print("el estado no esta bien")
-	driver.find_element_by_name("estado").clear()
-	driver.find_element_by_name("estado").send_keys("Capital Federal")
+element.send_keys("Capital Federal")
 
 #ingresar pais
 element = driver.find_element_by_name("pais")
-value = element.get_attribute("value")
-if value == "Argentina":
-	print("el pais esta bien")
-else:
-	print("el pais no esta bien")
-	driver.find_element_by_name("pais").clear()
-	driver.find_element_by_name("pais").send_keys("Argentina")
+element.send_keys("Argentina")
 
 #ingresar correro
 element = driver.find_element_by_name("correo")
-value = element.get_attribute("value")
-if value == "cliente@cliente.com":
-	print("el correo esta bien")
-else:
-	print("el correo no esta bien")
-	driver.find_element_by_name("correo").clear()
-	driver.find_element_by_name("correo").send_keys("cliente@cliente.com")  
+element.send_keys("cliente@cliente.com")  
 
 #ingresar direccion
 element = driver.find_element_by_name("direccion")
-value = element.get_attribute("value")
-if value == "calle falsa 123":
-	print("la direccion esta bien")
-else:
-	print("la direccion no esta bien")
-	driver.find_element_by_name("direccion").clear()
-	driver.find_element_by_name("direccion").send_keys("calle falsa 123")
-
-
+element.send_keys("calle falsa 123")
 
 #ingresar codigo postal
 element = driver.find_element_by_name("codigoPostal")
-value = element.get_attribute("value")
-if value == "1426":
-	print("el codigoPostal esta bien")
-else:
-	print("el codigoPostal no esta bien")
-	driver.find_element_by_name("codigoPostal").clear()
-	driver.find_element_by_name("codigoPostal").send_keys("1426")   
+element.send_keys("1426")   
 
 #ingresar termino de pago
 element = driver.find_element_by_name("terminoPago")
-value = element.get_attribute("value")
-if value == "12":
-	print("el termino de Pago esta bien")
-else:
-	print("el termino de Pago no esta bien")
-	driver.find_element_by_name("terminoPago").clear()
-	driver.find_element_by_name("terminoPago").send_keys("12") 
+element.send_keys("12") 
 
 #ingresar palabra traduccion
 element = driver.find_element_by_name("palabraTraduccion")
-value = element.get_attribute("value")
-if value == "20":
-	print("la palabra Traduccion esta bien")
-else:
-	print("la palabra Traduccion no esta bien")
-	driver.find_element_by_name("palabraTraduccion").clear()
-	driver.find_element_by_name("palabraTraduccion").send_keys("20") 
+element.send_keys("20") 
+
+#ingresar palabra edicion
+element = driver.find_element_by_name("palabraEdicion")
+element.send_keys("15") 
+
+#ingresar palabra Proofreading
+element = driver.find_element_by_name("palabraProofreading")
+element.send_keys("1") 
+
 
 #enviar cliente creado
 driver.find_element_by_class_name('boton-enviar').click()
--- crearUsuario.py
+++ crearUsuario.py
@@ -16,63 +16,27 @@ time.sleep(2)
  
 #editar nombre usuario
 element = driver.find_element_by_id("nombre")
-value = element.get_attribute("value")
-if value == "francsicoTest":
-    print("el nombre esta bien")
-else:
-    print("el nombre no esta bien")
-    driver.find_element_by_id("nombre").clear()
-    driver.find_element_by_id("nombre").send_keys("francsicoTest")
+element.send_keys("francsicoTest")
  
 #editar apellido
 element = driver.find_element_by_id("apellido")
-value = element.get_attribute("value")
-if value == "MigliardiTest":
-    print("el apellido esta bien")
-else:
-    print("el apellido no esta bien")
-    driver.find_element_by_id("apellido").clear()
-    driver.find_element_by_id("apellido").send_keys("MigliardiTest")
+element.send_keys("MigliardiTest")
  
 #editar contrasenia
 element = driver.find_element_by_id("contrsena")
-value = element.get_attribute("value")
-if value == "contrasena123":
-    print("la contrasena esta bien")
-else:
-    print("la contrasena no esta bien")
-    driver.find_element_by_id("contrsena").clear()
-    driver.find_element_by_id("contrsena").send_keys("contrasena123")
+element.send_keys("contrasena123")
  
 #confirmar contrasenia
 element = driver.find_element_by_id("contrsena2")
-value = element.get_attribute("value")
-if value == "contrasena123":
-    print("la contrasena esta bien")
-else:
-    print("la contrasena no esta bien")
-    driver.find_element_by_id("contrsena2").clear()
-    driver.find_element_by_id("contrsena2").send_keys("contrasena123")
+element.send_keys("contrasena123")
  
 #editar ciudad
 element = driver.find_element_by_id("nombreUsuario")
-value = element.get_attribute("value")
-if value == "FranTest":
-    print("el nombre de Usuario esta bien")
-else:
-    print("el nombre de Usuario no esta bien")
-    driver.find_element_by_id("nombreUsuario").clear()
-    driver.find_element_by_id("nombreUsuario").send_keys("Capital Federal")  
+element.send_keys("FranTest")  
  
 #editar correro
 element = driver.find_element_by_id("correo")
-value = element.get_attribute("value")
-if value == "frantest@fran.com":
-    print("el correo esta bien")
-else:
-    print("el correo no esta bien")
-    driver.find_element_by_id("correo").clear()
-    driver.find_element_by_id("correo").send_keys("frantest@fran.com")  
+element.send_keys("frantest@fran.com")  
  
  
 #enviar respuesta de usuario creado
